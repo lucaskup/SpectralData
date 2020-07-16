e841ece6b025eac04d9e8b196506560bc98fe908
lucaskup <LKUPSSINSKU@edu.unisinos.br>
2020-07-16 13:08:01 -0300

    Implemented change in select component

--- scripts/linechart.js
+++ scripts/linechart.js
@@ -32,6 +32,7 @@ var svg = d3
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.dsv(";", "data/ARE_2.csv").then(function (data) {
+  //var allGroup = ["ARE_21", "ARE_22", "ARE_23"];
   var x_val = [];
   var _keys = Object.keys(data[0]);
   _keys.forEach(function (d) {
@@ -39,21 +40,32 @@ d3.dsv(";", "data/ARE_2.csv").then(function (data) {
       x_val.push({
         band: parseFloat(d.replace(",", ".")),
         value: parseFloat(data[0][d].replace(",", ".")),
+        ARE_21: parseFloat(data[0][d].replace(",", ".")),
+        ARE_22: parseFloat(data[1][d].replace(",", ".")),
+        ARE_23: parseFloat(data[2][d].replace(",", ".")),
       });
     }
   });
+  const min_value = 0;
+
+  const max_value = d3.max([
+    d3.max(x_val, function (d) {
+      return d.ARE_21;
+    }),
+    d3.max(x_val, function (d) {
+      return d.ARE_22;
+    }),
+    d3.max(x_val, function (d) {
+      return d.ARE_23;
+    }),
+  ]);
   console.log(x_val);
   x.domain(
     d3.extent(x_val, function (d) {
       return d.band;
     })
   );
-  y.domain([
-    0,
-    d3.max(x_val, function (d) {
-      return d.value;
-    }),
-  ]);
+  y.domain([0, max_value]);
 
   svg
     .append("g")
@@ -72,7 +84,11 @@ d3.dsv(";", "data/ARE_2.csv").then(function (data) {
     .style("text-anchor", "end")
     .text("Number of Likes");
 
-  svg.append("path").datum(x_val).attr("class", "line").attr("d", line);
+  var lineComplete = svg
+    .append("path")
+    .datum(x_val)
+    .attr("class", "line")
+    .attr("d", line);
 
   var focus = svg.append("g").attr("class", "focus").style("display", "none");
 
@@ -118,18 +134,67 @@ d3.dsv(";", "data/ARE_2.csv").then(function (data) {
     .on("mousemove", mousemove);
 
   function mousemove() {
+    const actual_data = lineComplete.datum();
     var x0 = x.invert(d3.mouse(this)[0]),
-      i = bisectDate(x_val, x0, 1),
-      d0 = x_val[i - 1],
-      d1 = x_val[i],
+      i = bisectDate(actual_data, x0, 1),
+      d0 = actual_data[i - 1],
+      d1 = actual_data[i],
       d = x0 - d0.band > d1.band - x0 ? d1 : d0;
     focus.attr("transform", "translate(" + x(d.band) + "," + y(d.value) + ")");
     focus.select(".tooltip-date").text(d.band);
     focus.select(".tooltip-likes").text(formatValue(d.value));
   }
+
+  // A function that update the chart
+  function update(selectedGroup) {
+    // Create new data with the selection?
+    var dataFilter = x_val.map(function (d) {
+      return { band: d.band, value: d[selectedGroup] };
+    });
+    console.log(dataFilter);
+    console.log(line);
+    console.log("SVG");
+    console.log(svg);
+    // Give these new data to update line
+    lineComplete
+      .datum(dataFilter)
+      .transition()
+      .duration(1000)
+      .attr(
+        "d",
+        d3
+          .line()
+          .x(function (d) {
+            return x(+d.band);
+          })
+          .y(function (d) {
+            return y(+d.value);
+          })
+      );
+    console.log(lineComplete);
+
+    //svg.append("path").datum(x_val).attr("class", "line").attr("d", line);
+    //.datum(dataFilter)
+    //    .transition()
+    //    .duration(1000)
+    //.attr("stroke", function (d) {
+    //  return myColor(selectedGroup);
+    // });
+  }
+
+  // When the button is changed, run the updateChart function
+  d3.select("#select_sample").on("change", function (d) {
+    // recover the option that has been chosen
+    var selectedOption = d3.select(this).property("value");
+    // run the updateChart function with this selected option
+    update(selectedOption);
+  });
 });
 
-
-document.getElementById("select_sample").addEventListener("change", function(event){
+/*document
+  .getElementById("select_sample")
+  .addEventListener("change", function (event) {
     console.log(event);
+    const new_val = even.target.value;
   });
+*/
