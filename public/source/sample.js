32d63a61e26a3d76c2365a2eece88280127e4f88
lucaskup <LKUPSSINSKU@edu.unisinos.br>
2020-07-20 18:41:26 -0300

    Started sample class

--- public/source/sample.js
+++ public/source/sample.js
@@ -1,12 +1,29 @@
-"use strict"
+"use strict";
 
-class Sample{
-    constructor(name, point) {
-        this.name = name;
-        this.point = point;
-    }
-    legal(){
-        return "legal";
-    }
+class Sample {
+  constructor(name, point) {
+    this.name = name;
+    this.point = point;
+    this.spectra = [];
+  }
+
+  id() {
+    return this.name + "-" + this.point;
+  }
+  addToSpectra(band, value) {
+    this.spectra.push({ band, value });
+  }
+  sortSpectra() {
+    this.spectra = this.spectra.sort((a, b) => {
+      if (a.band > b.band) {
+        return 1;
+      }
+      if (a.band < b.band) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+  }
 }
 //const s = new Sample("Minha amostra",25);
