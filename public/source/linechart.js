21f8213aa3ddfa87c80b0e7d447fac12db508450
lucaskup <LKUPSSINSKU@edu.unisinos.br>
2020-07-20 23:22:52 -0300

    started to implement mvc, class sample

--- public/source/linechart.js
+++ public/source/linechart.js
@@ -57,7 +57,30 @@ let svg = d3
 // Espectro_Taubaté
 // Espectro_Torres
 
+/**
+ * Creates an array of samples from the csv data
+ * @param {*} data information read from d3 tsv function
+ */
+function createSamples(data) {
+  const samples = [];
+  // first two columns are reserved for sample name and reading point
+  const name_field = data.columns[0];
+  const point_field = data.columns[1];
+  // for each of the bands in the csv file
+  const bandColumns = data.columns.slice(2);
+  data.forEach((row) => {
+    const s = new Sample(row[name_field], row[point_field]);
+    bandColumns.forEach((band) => {
+      s.addToSpectra(band, row[band]);
+    });
+    samples.push(s);
+  });
+  return samples;
+}
+
 d3.dsv(";", "data/Espectro_Todos.csv").then(function (data) {
+  const samples = createSamples(data);
+  console.log(samples);
   let x_val = [];
   let hull = [];
   let continum = [];
@@ -340,78 +363,6 @@ function getContinumOpacity() {
   return d3.select("#chk_cont").property("checked") ? 1 : 0;
 }
 
-/**
- * Calculates the upper convex hull of the spectra passed on argument
- * @param {*} data Spectra that will be used to calculate the convex hull
- * @returns Convex Hull upper vertices
- */
-function calculateHull(data) {
-  let hull = [];
-  // the function d3.polygonHull expects a list of vertices
-  // (as its supposed to work for polygons)
-  // vertices list is created using a map function
-  const vertices = data.map((d) => [d.band, d.value]);
-
-  hull = d3.polygonHull(vertices);
-
-  hull.push(hull[0]);
-  const start_index = hull.findIndex((d) => d[0] == data[0].band);
-  hull = hull.slice(start_index, hull.length);
-  return hull;
-}
-/**
- * Removes the continum from data spectre
- * @param {*} data The reflectances with bands and values
- * @param {vertices} hull The upper convex hull of the data reflectances
- * @returns Spectra with continum removed: value / convex hull
- */
-function removeContinum(data, hull) {
-  const continumRemoved = [];
-  let indexHull = 0;
-  /*
-  the convex hull points are not as dense as
-   the radiometer data, but we can find linear
-  equations that describe each of the segments
-   of the convex hull
-  line equation f(x) = ax + b
-  */
-
-  // a = (f(x1) - f(x))/(x1 -x)
-  const angularCoeficientA = (idx) =>
-    (hull[idx + 1][1] - hull[idx][1]) / (hull[idx + 1][0] - hull[idx][0]);
-
-  // b = f(x) - a*x
-  const linearCoeficientB = (idx) =>
-    hull[idx][1] - angularCoeficientA(idx) * hull[idx][0];
-
-  //calculates the curified linear function in terms of hull index
-  const hullLinearFunction = (idx) => (x) =>
-    angularCoeficientA(idx) * x + linearCoeficientB(idx);
-
-  //finally f is the linear function
-  let f = hullLinearFunction(0);
-
-  //for (let i = 0; i < data.length; i++) {
-  data.forEach((reflectance) => {
-    /*
-    find the convexhull index, aka bewtween what points
-    in the convex hull the current value goes
-    eg: hullindexes [0,100,200]
-    valueindexes [0,1,2,...,199,200]
-    value index 45 is betwen hull indexes
-    [0] = 0 and [1] = 100
-    */
-    if (reflectance.band > hull[indexHull + 1][0]) {
-      indexHull++;
-      f = hullLinearFunction(indexHull);
-    }
-    // find the line
-    const x = reflectance.band;
-    continumRemoved.push([x, +(reflectance.value / f(x)).toFixed(4)]);
-  });
-
-  return continumRemoved;
-}
 function updateSelectOptions(sampleNames) {
   d3.select("#select_sample")
     .selectAll("option")
--- public/source/sample.js
+++ public/source/sample.js
@@ -1,18 +1,36 @@
 "use strict";
-
+/**
+ * @class
+ * Represents a sample point and its spectra
+ */
 class Sample {
   constructor(name, point) {
     this.name = name;
     this.point = point;
     this.spectra = [];
+    this.convexHull = undefined;
+    this.continumRemoved = undefined;
   }
-
+  /**
+   * @returns a unique id: name + "-" + point
+   */
   id() {
     return this.name + "-" + this.point;
   }
+  /**
+   * Adds a new wavelength to the sample spectra
+   * @param {String} band wavelength of the spectra
+   * @param {String} value % of reflectance
+   */
   addToSpectra(band, value) {
+    const convertBandValue = (x) => parseFloat(x.replace(",", "."));
+    band = convertBandValue(band);
+    value = convertBandValue(value);
     this.spectra.push({ band, value });
   }
+  /**
+   * Sorts the spectra according to the wavelength
+   */
   sortSpectra() {
     this.spectra = this.spectra.sort((a, b) => {
       if (a.band > b.band) {
@@ -25,5 +43,98 @@ class Sample {
       return 0;
     });
   }
+  /**
+   * This method has lazy processing implemented
+   * @returns upper convex of this sample spectra
+   */
+  getConvexHull() {
+    if (typeof this.convexHull === "undefined") {
+      this.convexHull = calculateHull(this.spectra);
+    }
+    return this.convexHull;
+  }
+  /**
+   * This method has lazy processing implemented
+   * @returns spectra with continum removed
+   */
+  getContinumRemovedSpectra() {
+    if (typeof this.continumRemoved === "undefined") {
+      this.continumRemoved = removeContinum(this.spectra, this.getConvexHull());
+    }
+    return this.continumRemoved;
+  }
 }
-//const s = new Sample("Minha amostra",25);
+
+/**
+ * Calculates the upper convex hull of the spectra passed on argument
+ * @param {*} data Spectra that will be used to calculate the convex hull
+ * @returns Convex Hull upper vertices
+ */
+function calculateHull(data) {
+  let hull = [];
+  // the function d3.polygonHull expects a list of vertices
+  // (as its supposed to work for polygons)
+  // vertices list is created using a map function
+  const vertices = data.map((d) => [d.band, d.value]);
+
+  hull = d3.polygonHull(vertices);
+
+  hull.push(hull[0]);
+  const start_index = hull.findIndex((d) => d[0] == data[0].band);
+  hull = hull.slice(start_index, hull.length);
+  return hull;
+}
+
+/**
+ * Removes the continum from data spectre
+ * @param {*} data The reflectances with bands and values
+ * @param {vertices} hull The upper convex hull of the data reflectances
+ * @returns Spectra with continum removed: value / convex hull
+ */
+function removeContinum(data, hull) {
+  const continumRemoved = [];
+  let indexHull = 0;
+  /*
+  the convex hull points are not as dense as
+   the radiometer data, but we can find linear
+  equations that describe each of the segments
+   of the convex hull
+  line equation f(x) = ax + b
+  */
+
+  // a = (f(x1) - f(x))/(x1 -x)
+  const angularCoeficientA = (idx) =>
+    (hull[idx + 1][1] - hull[idx][1]) / (hull[idx + 1][0] - hull[idx][0]);
+
+  // b = f(x) - a*x
+  const linearCoeficientB = (idx) =>
+    hull[idx][1] - angularCoeficientA(idx) * hull[idx][0];
+
+  //calculates the curified linear function in terms of hull index
+  const hullLinearFunction = (idx) => (x) =>
+    angularCoeficientA(idx) * x + linearCoeficientB(idx);
+
+  //finally f is the linear function
+  let f = hullLinearFunction(0);
+
+  //for (let i = 0; i < data.length; i++) {
+  data.forEach((reflectance) => {
+    /*
+    find the convexhull index, aka bewtween what points
+    in the convex hull the current value goes
+    eg: hullindexes [0,100,200]
+    valueindexes [0,1,2,...,199,200]
+    value index 45 is betwen hull indexes
+    [0] = 0 and [1] = 100
+    */
+    if (reflectance.band > hull[indexHull + 1][0]) {
+      indexHull++;
+      f = hullLinearFunction(indexHull);
+    }
+    // find the line
+    const x = reflectance.band;
+    continumRemoved.push([x, +(reflectance.value / f(x)).toFixed(4)]);
+  });
+
+  return continumRemoved;
+}
